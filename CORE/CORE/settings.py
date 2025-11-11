b5596fd852d35bb506f9d771a7c24e70c41090b3
Julioasmb <julioasmb@gmail.com>
2025-11-11 02:04:38 -0300

    Update allowed hosts and add requirements file
    Changed ALLOWED_HOSTS to include zerorague.pythonanywhere.com for deployment. Added requirements.txt to specify project dependencies.

--- CORE/CORE/settings.py
+++ CORE/CORE/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2vo*#x$67989odyt&h(a($#dg@nzn!lpqz#h(ip$m29e)-+xnc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["pythonanywhere.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["zerorague.pythonanywhere.com", "127.0.0.1"]
 
 
 # Application definition
